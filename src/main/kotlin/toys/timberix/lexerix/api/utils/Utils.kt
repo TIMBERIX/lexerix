fbb296b738048c67b3eeee5049cc6bf67a02fe9b
kberix <165256973+kberix@users.noreply.github.com>
2024-07-29 12:53:58 +0200

    add String.asCurrency() utility function

--- src/main/kotlin/toys/timberix/lexerix/api/utils/Utils.kt
+++ src/main/kotlin/toys/timberix/lexerix/api/utils/Utils.kt
@@ -1,7 +1,10 @@
+@file:Suppress("unused")
+
 package toys.timberix.lexerix.api.utils
 
 import java.math.BigDecimal
 import java.math.RoundingMode
 
 fun Float.asCurrency(): BigDecimal = BigDecimal.valueOf(toDouble()).setScale(2, RoundingMode.HALF_UP)
+fun String.asCurrency(): BigDecimal = BigDecimal(this).setScale(2, RoundingMode.HALF_UP)
 fun Float.asWeight(): BigDecimal = BigDecimal.valueOf(toDouble()).setScale(3, RoundingMode.HALF_UP)

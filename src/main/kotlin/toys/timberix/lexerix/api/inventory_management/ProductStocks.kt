74b76db1ec02af2cf594c7d6765acd02f51a9cc6
kberix <165256973+kberix@users.noreply.github.com>
2025-02-28 22:15:17 +0100

    change to productId (v0.3.1)

--- src/main/kotlin/toys/timberix/lexerix/api/inventory_management/ProductStocks.kt
+++ src/main/kotlin/toys/timberix/lexerix/api/inventory_management/ProductStocks.kt
@@ -5,7 +5,7 @@ package toys.timberix.lexerix.api.inventory_management
 import toys.timberix.lexerix.api.utils.DatedTable
 
 object ProductStocks : DatedTable("FK_LagerBestand") {
-    val artikelNr = reference("lArtikelId", Products.id)
+    val productId = reference("lArtikelId", Products.id)
     val stockAmount = integer("Bestand")
     val store = reference("lLagerId", Stores.id)
     val mengeBestellt = integer("Menge_bestellt").default(0)
